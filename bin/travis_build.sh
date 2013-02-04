1fdf5e88645b6a6d8dd0d4a35826d9c3b5718083
tom <tom@x2k.co.uk>
2013-02-04 21:01:09 +0000

    License check in travis

--- bin/travis_build.sh
+++ bin/travis_build.sh
@@ -1,12 +1,38 @@
 #!/bin/bash
 
+### Check coding standards
 vendor/bin/phpcs --standard=psr2 src
 PHPCS=$?
 
+### Check code quality
 vendor/bin/phpmd src text codesize
 PHPMD=$?
 
+### Check for license headers
+LICENSE=0
+
+LICENSE_HEADER="<?php
+/**
+ * SclSocket Module (https://github.com/SCLInternet/SclSocket)
+ *
+ * @link https://github.com/SCLInternet/SclSocket for the canonical source repository
+ * @license http://opensource.org/licenses/MIT The MIT License (MIT)
+ */"
+
+for i in `find src -name '*.php'`; do
+    diff <(echo "$LICENSE_HEADER") <(head -7 "$i");
+
+    if [ "$?" -ne "0" ]; then
+        echo "Missing or invalid license header in \"$i\""
+        let LICENSE=1
+    fi
+done
+
+### Display results
 EXIT=0
+
+echo
+echo "#### RESULTS:"
 
 if [ "$PHPCS" -ne "0" ]; then
     echo "**** Coding standards failed"
@@ -14,6 +40,10 @@ if [ "$PHPCS" -ne "0" ]; then
 fi
 if [ "$PHPCS" -ne "0" ]; then
     echo "**** Mess detection failed"
+    EXIT=1
+fi
+if [ "$LICENSE" -ne "0" ]; then
+    echo "**** License header check failed"
     EXIT=1
 fi
 
